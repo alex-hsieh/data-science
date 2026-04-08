e984f1382a0fc8adb0f2560b21e85591301684d7
alex-hsieh <130923939+alex-hsieh@users.noreply.github.com>
2026-04-07 23:27:52 -0400

    Add basic cleaning and preprocessing for dataset
    Strip whitespace from string columns, normalize missing-value marker '?' to NaN and drop incomplete rows. Encode the target 'income' as binary (1 for '>50K', else 0). Import numpy.nan and remove redundant columns ('fnlwgt' and 'education') to simplify features prior to modeling.

--- project2/prob3_knn/loan_prediction.py
+++ project2/prob3_knn/loan_prediction.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from numpy import nan
 
 # URL to source data
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
@@ -10,3 +11,18 @@ columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-
 
 # Fetch the data from the URL
 df = pd.read_csv(url, names=columns, sep=',\s*', engine='python')
+
+# stripping whitespace from string columns
+for col in df.select_dtypes(include=['object']).columns:
+    df[col] = df[col].str.strip()
+
+# handling the missing values represented by '?'
+df = df.replace('?', nan)
+df.isnull().sum()
+df.dropna(inplace=True)
+
+# encoding the target variable 'income' to binary values
+df['income'] = df['income'].apply(lambda x: 1 if x == '>50K' else 0)
+
+# dropping redudnant columns
+df.drop(['fnlwgt', 'education'], axis=1, inplace=True)
